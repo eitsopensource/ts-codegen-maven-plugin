2eca3e85b3fd8cc115cfb0fe662fb85a20307848
Eduardo Niehues <eduardo@edanni.io>
2017-11-14 17:40:30 -0200

    Removes stomp support

--- src/main/resources/templates/services-wrapper.ts
+++ src/main/resources/templates/services-wrapper.ts
@@ -1,7 +1,5 @@
 import { InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import * as SockJS from 'sockjs-client';
-import * as Stomp from '@stomp/stompjs';
 import { Observer } from 'rxjs/Observer';
 
 export interface MethodReturnTypes {
@@ -29,54 +27,7 @@ export let BROKER_CONFIGURATION = new InjectionToken<BrokerConfiguration>('broke
 /////////////////
 /////////////////
 
-type EngagedRealTimeMethods  = EngagedMethodData[];
-
-interface EngagedMethodData {
-    combinedName: string,
-    args: any[],
-    observable: Observable<any>,
-    observer: Observer<any>
-}
-
-interface RealTimeMethodsByEntity {
-    [entityName: string]: string[]
-}
-
-
-let stompClient: Stomp.Client;
-
-const serviceReturnTypes: ServiceReturnTypes = {};
-const realTimeMethodsByEntity: RealTimeMethodsByEntity = {};
-const engagedRealTimeMethods: EngagedRealTimeMethods = [];
-
-export function registerMethodTypes(serviceName: string, returnTypes: MethodReturnTypes) {
-    serviceReturnTypes[serviceName] = returnTypes;
-    Object.keys(returnTypes).forEach(methodName => {
-        const entityName = returnTypes[methodName];
-        if (!realTimeMethodsByEntity[entityName]) {
-            realTimeMethodsByEntity[entityName] = [];
-        }
-        realTimeMethodsByEntity[entityName].push(serviceName + '.' + methodName);
-    });
-}
-
 export function dwrWrapper(configuration: BrokerConfiguration, serviceName: string, methodName: string, ...args: any[]): Observable<any> {
-    if (configuration.realTime) {
-        const combinedName = serviceName + '.' + methodName;
-        const observable = Observable.create((observer: Observer<any>) => {
-            engagedRealTimeMethods.push({
-                combinedName,
-                args,
-                observer,
-                observable
-            });
-            lazyInitStompConnection(configuration).then(() => callEngagedMethod(configuration, serviceName, methodName, args, observer));
-            return () => {
-                delete engagedRealTimeMethods[combinedName];
-            }
-        });
-        return observable;
-    } else {
         return Observable.create((observer: Observer<any>) => {
             loadDwrIfNeeded(configuration).then(() => {
                 loadServiceIfNeeded(configuration, serviceName).then(service => {
@@ -95,7 +46,6 @@ export function dwrWrapper(configuration: BrokerConfiguration, serviceName: stri
                 });
             });
         });
-    }
 }
 
 function loadDwrIfNeeded(configuration: BrokerConfiguration): Promise<void> {
@@ -127,54 +77,5 @@ function loadServiceIfNeeded(configuration: BrokerConfiguration, name: string): 
             tag.onerror = () => resolve();
             document.body.appendChild(tag);
         }
-    });
-}
-
-function lazyInitStompConnection(configuration: BrokerConfiguration): Promise<void> {
-    return new Promise<void>(resolve => {
-        if(stompClient && stompClient.connected) {
-            resolve();
-        } else {
-            stompClient = Stomp.over(new SockJS(configuration.stompPath) as any);
-            stompClient.connect({}, () => {
-                stompClient.subscribe('/topic/entities', message => handleStompMessage(configuration, message));
-                resolve();
-            }, () => setTimeout(() => lazyInitStompConnection(configuration), 5000));
-        }
-    });
-}
-
-function handleStompMessage(configuration: BrokerConfiguration, message: Stomp.Message) {
-    const fullType: string = message.body;
-
-    realTimeMethodsByEntity[fullType].forEach(combinedName => {
-        const [service, method] = combinedName.split('.');
-        engagedRealTimeMethods.filter(data => data.combinedName === service + '.' + method)
-            .forEach(data => {
-                const {args, observer} = data;
-                callEngagedMethod(configuration, service, method, args, observer);
-            });
-    });
-}
-
-function callEngagedMethod(configuration: BrokerConfiguration, serviceName: string, methodName: string, args: any[], observer: Observer<any>) {
-    loadDwrIfNeeded(configuration).then(() => {
-        loadServiceIfNeeded(configuration, serviceName).then(service => {
-            const callback = {
-                callback(result) {
-                    observer.next(result);
-                },
-                errorHandler(message, exception) {
-                    observer.error({ message, exception });
-                    console.error(message, exception);
-                }
-            };
-            if(args[args.length - 1].callback) {
-                args[args.length - 1] = callback;
-            } else {
-                args.push(callback);
-            }
-            service[methodName].apply(service, args);
-        });
     });
 }
