113c443de9237291585ff5909b366083d291df6d
Eduardo Antonio Niehues <eduardo@edanni.io>
2018-03-09 00:36:26 -0300

    Correções

--- src/main/resources/templates/services-wrapper.ts
+++ src/main/resources/templates/services-wrapper.ts
@@ -8,7 +8,8 @@ import { Observer } from 'rxjs/Observer';
 export interface BrokerConfiguration {
     path: string,
     realTime: boolean,
-    stompPath?: string
+    stompPath?: string,
+    fixTz?: boolean
 }
 
 export let BROKER_CONFIGURATION = new InjectionToken<BrokerConfiguration>('broker.configuration');
@@ -20,55 +21,57 @@ export let BROKER_CONFIGURATION = new InjectionToken<BrokerConfiguration>('broke
 /////////////////
 
 export function dwrWrapper(configuration: BrokerConfiguration, serviceName: string, methodName: string, ...args: any[]): Observable<any> {
-        return Observable.create((observer: Observer<any>) => {
-            loadDwrIfNeeded(configuration).then(() => {
-                loadServiceIfNeeded(configuration, serviceName).then(service => {
-                    args.push({
-                        callback(result) {
-                            observer.next(result);
-                            observer.complete();
-                        },
-                        errorHandler(message, exception) {
-                            observer.error({ message, exception });
-                            console.error(message, exception);
-                            observer.complete();
-                        }
-                    });
-                    service[methodName].apply(service, args);
+    return Observable.create((observer: Observer<any>) => {
+        loadDwrIfNeeded(configuration).then(() => {
+            loadServiceIfNeeded(configuration, serviceName).then(service => {
+                args.push({
+                    callback(result) {
+                        observer.next(result);
+                        observer.complete();
+                    },
+                    errorHandler(message, exception) {
+                        observer.error({ message, exception });
+                        console.error(message, exception);
+                        observer.complete();
+                    }
                 });
+                service[methodName].apply(service, args);
             });
         });
+    });
 }
 
 function loadDwrIfNeeded(configuration: BrokerConfiguration): Promise<void> {
     return new Promise<void>(resolve => {
         if ((window as any).dwr) {
-            shimDwrThenResolve(resolve);
+            shimDwrThenResolve(configuration, resolve);
         } else {
             const path = `${configuration.path}/engine.js`;
             const tag: HTMLScriptElement = document.createElement('script');
             tag.src = path;
             tag.type = 'text/javascript';
-            tag.onload = () => shimDwrThenResolve(resolve);
-            tag.onerror = () => shimDwrThenResolve(resolve);
+            tag.onload = () => shimDwrThenResolve(configuration, resolve);
+            tag.onerror = () => shimDwrThenResolve(configuration, resolve);
             document.body.appendChild(tag);
         }
     });
 }
 
-function shimDwrThenResolve(resolve: Function) {
-    (function(dwr) {
-        const original = dwr.engine.serialize.convert;
-        dwr.engine.serialize.convert = function(batch, directrefmap, otherrefmap, data, name, depth) {
-            if(data != null && typeof data == "object" && Object.prototype.toString.call(data) == "[object Date]") {
-                const offset = data.getTimezoneOffset()*60*1000;
-                const newDate = new Date(data.getTime() - offset);
-                original(batch, directrefmap, otherrefmap, newDate, name, depth);
-            } else {
-                original(batch, directrefmap, otherrefmap, data, name, depth);
-            }
-        }
-    })(window['dwr']);
+function shimDwrThenResolve(configuration: BrokerConfiguration, resolve: Function) {
+    if (configuration.fixTz) {
+        (function(dwr) {
+            const original = dwr.engine.serialize.convert;
+            dwr.engine.serialize.convert = function(batch, directrefmap, otherrefmap, data, name, depth) {
+                if (data != null && typeof data == 'object' && Object.prototype.toString.call(data) == '[object Date]') {
+                    const offset = data.getTimezoneOffset() * 60 * 1000;
+                    const newDate = new Date(data.getTime() - offset);
+                    original(batch, directrefmap, otherrefmap, newDate, name, depth);
+                } else {
+                    original(batch, directrefmap, otherrefmap, data, name, depth);
+                }
+            };
+        })(window['dwr']);
+    }
     resolve();
 }
 
