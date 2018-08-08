a8bb7f76aacb24575cca3f40c6cc8a30903bc149
Eduardo Antonio Niehues <eduardo@edanni.io>
2018-08-07 23:18:38 -0300

    Atualizações para Angular 6

--- src/main/resources/templates/services-wrapper.ts
+++ src/main/resources/templates/services-wrapper.ts
@@ -1,15 +1,11 @@
 import { InjectionToken } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
+import { Observable, Observer } from 'rxjs';
 
 /**
  * path é o caminho SEM BARRA AO FINAL para o broker. Por padrão é simplesmente 'broker'
  */
 export interface BrokerConfiguration {
-    path: string,
-    realTime: boolean,
-    stompPath?: string,
-    fixTz?: boolean
+    path: string
 }
 
 export let BROKER_CONFIGURATION = new InjectionToken<BrokerConfiguration>('broker.configuration');
@@ -44,36 +40,19 @@ export function dwrWrapper(configuration: BrokerConfiguration, serviceName: stri
 function loadDwrIfNeeded(configuration: BrokerConfiguration): Promise<void> {
     return new Promise<void>(resolve => {
         if ((window as any).dwr) {
-            shimDwrThenResolve(configuration, resolve);
+            resolve();
         } else {
             const path = `${configuration.path}/engine.js`;
             const tag: HTMLScriptElement = document.createElement('script');
             tag.src = path;
             tag.type = 'text/javascript';
-            tag.onload = () => shimDwrThenResolve(configuration, resolve);
-            tag.onerror = () => shimDwrThenResolve(configuration, resolve);
+            tag.onload = () => resolve();
+            tag.onerror = () => resolve();
             document.body.appendChild(tag);
         }
     });
 }
 
-function shimDwrThenResolve(configuration: BrokerConfiguration, resolve: Function) {
-    if (configuration.fixTz) {
-        (function(dwr) {
-            const original = dwr.engine.serialize.convert;
-            dwr.engine.serialize.convert = function(batch, directrefmap, otherrefmap, data, name, depth) {
-                if (data != null && typeof data == 'object' && Object.prototype.toString.call(data) == '[object Date]') {
-                    const offset = data.getTimezoneOffset() * 60 * 1000;
-                    const newDate = new Date(data.getTime() - offset);
-                    original(batch, directrefmap, otherrefmap, newDate, name, depth);
-                } else {
-                    original(batch, directrefmap, otherrefmap, data, name, depth);
-                }
-            };
-        })(window['dwr']);
-    }
-    resolve();
-}
 
 function loadServiceIfNeeded(configuration: BrokerConfiguration, name: string): Promise<any> {
     return new Promise<any>(resolve => {
--- src/main/resources/templates/services.template.ts
+++ src/main/resources/templates/services.template.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { BROKER_CONFIGURATION, BrokerConfiguration, dwrWrapper } from './services-wrapper';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { @IMPORTS@ } from './entities';
 
 @SERVICES@
